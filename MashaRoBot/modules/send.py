93c6d5a6893894e42fe88e20112ca2133cc43097
Adhi <73607487+piku-adhi@users.noreply.github.com>
2021-07-28 22:08:11 +0530

    Update send.py

--- MashaRoBot/modules/send.py
+++ MashaRoBot/modules/send.py
@@ -8,7 +8,7 @@ from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
 
 @run_async
 @dev_plus
-def reply(update, context):
+def send(update, context):
     args = update.effective_message.text.split(None, 1)
     creply = args[1]
     send_message.reply_to_message(update.effective_message, creply)
