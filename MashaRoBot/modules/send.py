67cd259971d092dcfd91eeaa7513b380557bfdcd
Adhi <73607487+piku-adhi@users.noreply.github.com>
2021-07-28 22:17:24 +0530

    Update send.py

--- MashaRoBot/modules/send.py
+++ MashaRoBot/modules/send.py
@@ -2,7 +2,7 @@ from telegram.ext import run_async
 
 from MashaRoBot import dispatcher
 from MashaRoBot.modules.disable import DisableAbleCommandHandler
-from MashaRoBot.modules.helper_funcs.alternate import send_message
+from MashaRoBot.modules.helper_funcs.alt import send_message
 from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
 
 
@@ -11,7 +11,7 @@ from MashaRoBot.modules.helper_funcs.chat_status import dev_plus
 def send(update, context):
     args = update.effective_message.text.split(None, 1)
     creply = args[1]
-    send_message.reply_to_message(update.effective_message, creply)
+    send_message(update.effective_message, creply)
 
 
 ADD_CCHAT_HANDLER = DisableAbleCommandHandler("kitty", send)
