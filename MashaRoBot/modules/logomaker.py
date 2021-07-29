a12e2000ac73e7bf2c5a8879158aaee25cd10121
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-07-29 13:12:43 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -31,7 +31,7 @@ async def lego(event):
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/4
+    x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByYone.png"
@@ -60,13 +60,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./YoneRobot/resources/blackbg.jpg')
+    img = Image.open('./MashaRoBot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 500)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -76,7 +76,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByisabella.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @rosebakthan")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @kittysupport❤")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
