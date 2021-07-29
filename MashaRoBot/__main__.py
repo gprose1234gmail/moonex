b10f3d9b3d17c3def143df291d94c89779322867
Adhi <73607487+piku-adhi@users.noreply.github.com>
2021-07-29 20:28:48 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -317,7 +317,7 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="「 GO BACK 」", callback_data="help_back")]]
+                    [[InlineKeyboardButton(text="「 GO BACK ❖", callback_data="help_back")]]
                 ),
             )
 
