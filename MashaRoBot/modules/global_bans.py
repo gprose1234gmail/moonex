a13dbbd73c5a0e66fbc33b1724db6ac78af2e356
Adhi <73607487+piku-adhi@users.noreply.github.com>
2021-07-30 12:51:35 +0530

    Update global_bans.py

--- MashaRoBot/modules/global_bans.py
+++ MashaRoBot/modules/global_bans.py
@@ -257,7 +257,7 @@ def gban(update: Update, context: CallbackContext):
 
     if gban_time > 60:
         gban_time = round((gban_time / 60), 2)
-        message.reply_text("Done! Gbanned.", parse_mode=ParseMode.HTML)
+        message.reply_text("Done! Gbanned in <code>{gbanned_chats}</code> chats .", parse_mode=ParseMode.HTML)
     else:
         message.reply_text("Done! Gbanned.", parse_mode=ParseMode.HTML)
 
