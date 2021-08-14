0de887d30a552ea0e77229d2df6fa82cf1495feb
Dhanush༆143 <77870791+pondhanush764@users.noreply.github.com>
2021-08-14 12:26:42 +0530

    Update misc.py

--- MashaRoBot/modules/misc.py
+++ MashaRoBot/modules/misc.py
@@ -105,14 +105,14 @@ Output: `1.0 USD = 75.505 INR`
 
 """
 
-ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
+ECHO_HANDLER = DisableAbleCommandHandler("say", echo, filters=Filters.group)
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
 __mod_name__ = "EXTRAS"
-__command_list__ = ["id", "echo"]
+__command_list__ = ["id", "say"]
 __handlers__ = [
     ECHO_HANDLER,
     MD_HELP_HANDLER,
