582a04a795b6b04147a86a649085b6dc5816f649
Dhanush༆143 <77870791+pondhanush764@users.noreply.github.com>
2021-08-16 18:36:38 +0530

    Create vcmusic.py

---
+++ MashaRoBot/modules/vcmusic.py
@@ -0,0 +1,15 @@
+__help__="""
+❍/play <song name> - play song you requested
+❍/dplay <song name> - play song you requested via deezer
+❍/splay <song name> - play song you requested via jio saavn
+❍/playlist - Show now playing list/current - Show now playing
+*Admins only*
+❍/player - open music player settings panel
+❍/pause - pause song play
+❍/resume - resume song play
+❍/skip - play next song
+❍/end - stop music play
+❍/userbotjoin - invite assistant to your chat
+❍/admincache - Refresh admin list
+
+__Mod_name__="Vc music🥰"
